a5d5d6ac5a56b6e48985bdaa37f52ccca7c1d32e
leyno69 <oscarleynaud@gmail.com>
2026-07-30 01:41:24 +0200

    S2.1: expose financial domain primitives

---
+++ src/domain/financial/index.mjs
@@ -0,0 +1,6 @@
+export { FinancialDomainError } from './errors.mjs';
+export { createAccount } from './account.mjs';
+export { createCalculatedPosition } from './calculated-position.mjs';
+export { createInstrument } from './instrument.mjs';
+export { createTemporal } from './temporal.mjs';
+export { createTransaction } from './transaction.mjs';
