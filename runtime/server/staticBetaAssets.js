42ff14bccafc9672f099016f8eb70b75a9b6a090
leyno69 <oscarleynaud@gmail.com>
2026-07-31 21:53:00 +0200

    fix(server): define beta static asset manifest

---
+++ runtime/server/staticBetaAssets.js
@@ -0,0 +1,17 @@
+export const STATIC_BETA_ASSETS = Object.freeze([
+  ['leynor-brand.js', 'text/javascript; charset=utf-8'],
+  ['leynor-logo.js', 'text/javascript; charset=utf-8'],
+  ['leynor-brand.css', 'text/css; charset=utf-8'],
+  ['pwa.js', 'text/javascript; charset=utf-8'],
+  ['pwa.css', 'text/css; charset=utf-8'],
+  ['manifest.webmanifest', 'application/manifest+json; charset=utf-8'],
+  ['service-worker.js', 'text/javascript; charset=utf-8'],
+  ['api-fetch-router.js', 'text/javascript; charset=utf-8'],
+  ['api-connection.js', 'text/javascript; charset=utf-8'],
+  ['leynor-assistant.js', 'text/javascript; charset=utf-8'],
+  ['leynor-conversation.js', 'text/javascript; charset=utf-8'],
+  ['leynor-financial-cards.js', 'text/javascript; charset=utf-8'],
+  ['opportunity-radar-ui.js', 'text/javascript; charset=utf-8'],
+  ['icons/leynor-icon.svg', 'image/svg+xml'],
+  ['icons/leynor-maskable.svg', 'image/svg+xml']
+]);
